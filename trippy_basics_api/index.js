cd3476fcf1d3d36a841f1b9b807ef64a78d312aa
Schenkel <thi.schenkel@gmail.com>
2021-03-15 15:33:57 +0100

    put hotel and restaurant

--- trippy_basics_api/index.js
+++ trippy_basics_api/index.js
@@ -39,29 +39,55 @@ app.post('/hotels', (req, res) => {
 });
 
 // Ajouter la possiblité de mettre à jour le nom d’un hôtel (PUT /hotels/:id?name=newName) ==> A REVOIR
-// app.put('/hotels/:id?name=newName', (req, res) => {
-//     const hotel = new hotelModel({
-//         ...req.body
-//     });
-//     hotel.updateOne({ _id: req.query.id }, { ...req.body, _id: req.query.name })
-//         .then(() => res.status(200).json({ message: 'Nom Hotel modifié!' }))
-//         .catch(error => res.status(400).json({ error }));
-// });
+app.put('/hotels/:id?name=newName', (req, res) => {
+    const hotel = new hotelModel({
+        ...req.body
+    });
+    hotel.updateOne({ _id: req.query.id }, { ...req.body, _id: req.query.name })
+        .then(() => res.status(200).json({ message: 'Nom Hotel modifié!' }))
+        .catch(error => res.status(400).json({ error }));
+});
 
 // Ajouter la possiblité d’effacer un hôtel (`DELETE /hotels/:id`)
-// app.delete('/hotels/:id', async (req, res) => {
-//     const hotel = await hotelModel.deleteOne({ _id: req.params.id }).lean().exec();
-//     res.json(hotel);
-//     res.send({ message: 'Hotel supprimé!' });
-// });
+app.delete("/hotels/:id", async (req, res) => {
+    await hotelModel.deleteOne({ _id: req.params.id });
+    res.send(`hotel kaput`);
+});
 
+
+
+
+
+//Créer la route `/restaurants` qui retournera tous les restaurants (`GET /restaurants`)
 app.get('/restaurants', async (req, res) => {
     const restaurant = await restaurantModel.find(); res.json(restaurant);
 });
+
+// Créer la route /restaurants/:id où l’ID correspondra à l’Object Id dans la base de données (GET /restaurants/:id)
 app.get('/restaurants/:id', async (req, res) => {
     const restaurant = await restaurantModel.find({ _id: req.params.id }).lean().exec(); res.json(restaurant);
 });
 
+// Ajouter la possibilité de créer un nouveau restaurant (POST /restaurants)
+app.post('/restaurants', (req, res) => {
+    const restaurant = new restaurantModel({
+        ...req.body
+    });
+    restaurant.save()
+        .then(() => res.status(201).json({ message: 'Nouvel restaurant enregistré!' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+// Ajouter la possiblité de mettre à jour le nom d’un restaurant (`PUT /restaurants?name=newName`)
+
+
+
+
+// Ajouter la possiblité d’effacer un restaurant (`DELETE /restaurants/:id`)
+app.delete("/restaurants/:id", async (req, res) => {
+    await restaurantModel.deleteOne({ _id: req.params.id });
+    res.send(`restaurant kaput`);
+});
 
 
 
